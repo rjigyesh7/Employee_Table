81795fa6b1dae2f8ce9c30f8f363fdfd91bd738a
rjigyesh7 <rtdjigyesh777@gmail.com>
2020-12-30 17:36:24 +0530

    Minor update

---
+++ src/test/java/com/app/employee/spring/SpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.employee.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
